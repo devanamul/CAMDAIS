9a1a9f9879fc90a9ee3d36137175d00fa917e3b2
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-03-08 01:45:15 +0600

    app installed at settings.py file

--- CAMDAIS/CAMDAIS/settings.py
+++ CAMDAIS/CAMDAIS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CAMDAIS.app.CAMDAISConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
