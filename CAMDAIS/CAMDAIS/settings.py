c4620a1f530d494ca34939b9ed812e7414fcec22
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-03-26 16:37:18 +0600

    1 line edited

--- CAMDAIS/CAMDAIS/settings.py
+++ CAMDAIS/CAMDAIS/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'CAMDAIS.app.CAMDAISConfig'
+    'CAMDAIS.app.CAMDAISConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
