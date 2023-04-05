65d66d45ece6925408e14c610c8734ac97f37513
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-04-06 00:00:39 +0600

    done some changes

--- CAMDAIS/CAMDAIS/views.py
+++ CAMDAIS/CAMDAIS/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "CAMDAIS/home.html")
+
+def signin(request):
+	if request.method == 'GET':
+		return render(request, 'CAMDAIS/login.html')
