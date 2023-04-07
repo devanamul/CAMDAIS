eabf8980717707412fdf5f35f2f493e3b66c3c05
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-04-07 20:24:50 +0600

    model created for system admin

--- CAMDAIS/CAMDAIS/models.py
+++ CAMDAIS/CAMDAIS/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 class institute(models.Model):
 	name = models.CharField(max_length=150)
 	status = models.IntegerField()
+class systemAdmin(models.Model):
+	institute = models.ForeignKey(institute, on_delete=models.CASCADE)
+	author = author = models.ForeignKey(User, on_delete=models.CASCADE)
+	status = models.IntegerField()
