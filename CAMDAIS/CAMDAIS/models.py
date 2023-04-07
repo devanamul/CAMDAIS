5a9e8b94c67ab9fe5e36972341e0ec268943ee12
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-04-07 20:39:31 +0600

    model created for student

--- CAMDAIS/CAMDAIS/models.py
+++ CAMDAIS/CAMDAIS/models.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import User
 class institute(models.Model):
 	name = models.CharField(max_length=150)
 	status = models.IntegerField()
+
 class systemAdmin(models.Model):
 	institute = models.ForeignKey(institute, on_delete=models.CASCADE)
 	author = author = models.ForeignKey(User, on_delete=models.CASCADE)
+	status = models.IntegerField()
+
+class student(models.Model):
+	institute = models.ForeignKey(institute, on_delete=models.CASCADE)
+	author = author = models.ForeignKey(User, on_delete=models.CASCADE)
+	level = models.IntegerField()
 	status = models.IntegerField()
