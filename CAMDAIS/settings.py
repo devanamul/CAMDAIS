49e6b9c21128d906e6f81b82e2c6049aefa490e4
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-05-17 03:42:47 +0600

    host updated

--- CAMDAIS/settings.py
+++ CAMDAIS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%*y)h7fj@o)xm7em-lpqa)(*$94r$i57y0w489!7)%)$=gd7#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'camdais.azurewebsites.net']
 
 
 # Application definition
